6d83d8b9a53aacc1fbe297dbd200fba7fbf57f0a
이민석 <minseok.happy.lee@gmail.com>
2021-09-16 18:54:36 +0000

    최초 경로 전체 상품리스트 조회

--- src/main/java/dev/practice/order/view/item/MainController.java
+++ src/main/java/dev/practice/order/view/item/MainController.java
@@ -5,13 +5,18 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
-    @GetMapping(value = "/view/v1/items/all")
+    @GetMapping(value = "/")
     public String main() {
         return "order/items/all";
     }
 
+    @GetMapping(value = "/view/v1/items/all")
+    public String orderItemsAll() {
+        return "order/items/all";
+    }
+
     @GetMapping(value = "/view/v1/items/{itemToken}")
-    public String main(@PathVariable("itemToken") String itemToken) {
+    public String orderItemsItemtoken(@PathVariable("itemToken") String itemToken) {
         return "order/items/item-token";
     }
 }
